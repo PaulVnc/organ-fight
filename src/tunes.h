c4fc7e31db2078143851d8525ad9e5288cba8a11
Paul VANCAUWENBERGHE <paul.vancauwenberghe@telecom-sudparis.eu>
2022-06-08 12:03:06 +0200

    git apprend la casse un jour stp

--- src/tunes.h
+++ src/tunes.h
@@ -5,6 +5,7 @@ class Tunes {
 private:
 	float time;
 	std::string tune;
+
 public:
 	Tunes(float time, std::string tune);
 	Tunes(std::string tune, float beat, int id_mesure, float chiffrage, float tempo);
