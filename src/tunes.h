cd55d8313e2430de3f7c4278f012977e02a55c75
Skizaat <77202823+Skizaat@users.noreply.github.com>
2022-06-09 15:48:39 +0200

    add commentaries for tunes

--- src/tunes.h
+++ src/tunes.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <string>
 
+/* Cette classe représente les notes parsées du Json de la partition.
+Le champs time: c'est le temps absolu (en secondes) à laquelle la note doit être jouée
+	ex: si time = 9.05 alors la note doit être jouée 9.05s après le début de la partition
+Le champs tune: c'est une string contenant le nom de la note (en anglais) à jouer
+	ex: si tune = B alors la note est un Si
+	
+Ainsi, l'enchainement de Tunes représente le mélodie écrite textuellement dans le Json de la partition*/
+
 class Tunes {
 private:
 	float time;
