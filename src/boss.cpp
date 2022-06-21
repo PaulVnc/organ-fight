19157093ea93e3cb2a8b1863ba3f2da75251ccc7
Skizaat <77202823+Skizaat@users.noreply.github.com>
2022-06-21 15:49:17 +0200

    c'est trop chelou git aled

--- src/boss.cpp
+++ src/boss.cpp
@@ -1,50 +1,29 @@
-#include "box2d/box2d.h"
-#include <stdio.h>
-#include <assert.h>
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <SFML/Audio/Sound.hpp>
-#include <SFML/Audio/SoundBuffer.hpp>
-#include "boss.h"
+#include "Boss.h"
 
-int time_count=0;
-
-Boss::Boss(int initial_health,b2World* world, sf::Texture& texture)
-	:MovingObject(b2Vec2(15.0f, -20.0f), b2Vec2(0, 0), 2.0f, 2.0f, world, texture)
-	, health(initial_health)
-{
+void Boss::setRectangle(float width, float height) {
+    sf::RectangleShape* rectangle = new sf::RectangleShape(sf::Vector2f(width, height));
+    rectangle->setPosition(position);
+    rectangle->setOrigin(width / 2, height / 2);
+    rectangle->setFillColor(sf::Color::Blue);
+    shape = rectangle;
 }
 
-void Boss::changeDirection() {
-	int pattern = rand() % 4;
-	switch (pattern) {
-	case 0:
-		SetVelocity(b2Vec2(0.0f, 0.0f));
-		break;
-	case 1:
-		SetVelocity(b2Vec2(0.0f, 0.1f));
-		break;
-	case 2:
-		SetVelocity(b2Vec2(0.0f, 0.0f));
-		break;
-	case 3:
-		SetVelocity(b2Vec2(0.0f, -0.1f));
-		break;
-	}
+void Boss::createBody(b2World& world) {
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(position.x / worldScale, position.y / worldScale);
+    body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape polygonShape;
+    polygonShape.SetAsBox(shape->getLocalBounds().width / 2 / worldScale, shape->getLocalBounds().height / 2 / worldScale);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &polygonShape;
+    //fixtureDef.density = dynamic;
+    fixtureDef.friction = 0.3f;
+    body->CreateFixture(&fixtureDef);
 }
 
-int Boss::bossMain() {
-	time_count = (time_count + rand() % 2) % 1000;
-	if (time_count == 0) {
-		changeDirection();
-	}
-	SetVelocity(b2Vec2(0.0f, GetVelocity().y));
-	if (GetVelocity().y < 0.0f && GetPosition().y < -24.0f ) {
-		SetVelocity(b2Vec2(0.0f, 0.0f));
-	}
-
-	if (GetVelocity().y > 0.0f && GetPosition().y > -18.0f) {
-		SetVelocity(b2Vec2(0.0f, 0.0f));
-	}
-	return 0;
+void Boss::update() {
+    shape->setPosition(body->GetPosition().x * worldScale, body->GetPosition().y * worldScale);
 }
--- src/boss.h
+++ src/boss.h
@@ -1,16 +1,18 @@
-#pragma once
 #include "box2d/box2d.h"
-#include "movingObject.h"
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 
-class Boss:public MovingObject {
+
+class Boss
+{
 private:
-	int health;
-	bool reflectMode = false;
+	sf::Vector2f position;
+	sf::Shape* shape;
+	b2Body* body;
+	const int worldScale = 30;
 public:
-	//void setReflectMode(bool state);
-	//void hit(int amount);
-	Boss(int iniitial_health, b2World* world, sf::Texture& texture);
-	void changeDirection();
-	int bossMain();
+	Boss(float x, float y) : position(x, y) {};
+	void setRectangle(float width, float height);
+	void createBody(b2World& world);
+	void update();
+	sf::Shape& getShape() { return *shape; };
 };
