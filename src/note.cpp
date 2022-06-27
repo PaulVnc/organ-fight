4a762544022a3065f5fe6d7eca52d7386647d84a
Skizaat <77202823+Skizaat@users.noreply.github.com>
2022-06-27 15:57:34 +0200

    change notes spawning to avoid lag

--- src/note.cpp
+++ src/note.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 Note::Note(float x_pos, std::string note, int wnf, b2World* world, float RATIO, sf::Texture& texture, int nuance)
-	:MovingObject(b2Vec2(x_pos,5.0f),b2Vec2(0.0f,-0.5f), 0.5f,0.5f,world, texture)
+	:MovingObject(b2Vec2(x_pos,0.0f),b2Vec2(0.0f,-0.5f), 0.5f,0.5f,world, texture)
 	,sound(note)
 	,whole_note_frac(wnf)
 	,nuance(nuance)
